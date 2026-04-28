52251b9bcf6d37bd790d5298c4c5898ac89aeb76
hshehriyaralam <hshehriyaralam@gmail.com>
2026-04-29 01:16:52 +0500

    integrate Live preview html & css, polish UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,159 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useState } from "react";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
 export default function Home() {
+  const [html, setHtml] = useState("");
+  const [css, sectCss] = useState("");
+  const [js, setJs] = useState("");
+  const [srcDoc, setSrcDoc] = useState<any>([]);
+  const [consoleOutput, setConsoleOutput] = useState("");
+
+  const RunCode = () => {
+    const code = `<html lang="en">
+        <head>
+        <style>
+        ${css}
+        </style>
+      <body>
+          ${html}
+      </body>
+      <script type="text/javascript">
+      ${js}
+      </script>
+      </html>`;
+    setSrcDoc(code);
+  };
+
+  console.log(consoleOutput);
+  useEffect(() => {
+    setTimeout(() => {
+      RunCode();
+    }, 500);
+  }, [html, css, js]);
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <div className="w-full min-h-screen bg-white ">
+      <div className="flex items-center justify-between mx-10  my-2">
+        <h1 className="text-4xl  font-quicksand font-bold  text-teal-600 text-center  mb-4">
+          CodeJS
+        </h1>
+        <button
+          onClick={RunCode}
+          className="py-2  px-3 py-1  text-md rounded-xl text-gray-100
+       cursor-pointer bg-teal-500  font-bold "
+        >
+          Code Run
+        </button>
+      </div>
+
+      {/* Editor */}
+
+      <ResizablePanelGroup
+        orientation="horizontal"
+        className="p-2  flex items-center    w-full   "
+      >
+        {/* for html */}
+        <ResizablePanel
+          defaultSize="32%"
+          className="border  border-gray-200 rounded-xl   w-120 min-h-90 scroll-none"
+        >
+          <h2 className="text-[28px]  font-bold  m-3  text-center text-mauve-600">
+            HTML
+          </h2>
+
+          <textarea
+            className="text-gray-700  w-[90%]  mx-4  h-70
+            outline-none  border-none     text-[15px]  resize-none"
+            value={html}
+            onChange={(e) => setHtml(e.target.value)}
+            rows={4}
+          ></textarea>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+
+        {/* for CSS */}
+        <ResizablePanel
+          defaultSize="33%"
+          className="border  border-gray-200 rounded-xl   w-120 min-h-90     "
+        >
+          <h2 className="text-[28px]  font-bold  m-3  text-center  text-mauve-600">
+            CSS
+          </h2>
+
+          <textarea
+            className="text-gray-700  w-[90%]  mx-4 h-70
+            outline-none  border-none      text-[15px]  resize-none"
+            value={css}
+            onChange={(e) => sectCss(e.target.value)}
+            rows={4}
+          ></textarea>
+        </ResizablePanel>
+
+        <ResizableHandle withHandle />
+
+        {/* for JS */}
+        <ResizablePanel
+          defaultSize="33%"
+          className="border  border-gray-200 rounded-xl   w-120 min-h-90    "
+        >
+          <h2 className="text-[28px]  font-bold  m-3  text-center text-mauve-600">
+            JavaScript
+          </h2>
+
+          <textarea
+            className="text-gray-700  w-[90%]  mx-4  h-70
+            outline-none  border-none    text-[15px]  resize-none"
+            value={js}
+            onChange={(e) => setJs(e.target.value)}
+            rows={4}
+          ></textarea>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+
+      {/* Previews  */}
+      <ResizablePanelGroup
+        orientation="vertical"
+        className="min-h-[500px] max-w-sm rounded-lg border  max-w-full"
+      >
+        <ResizablePanel
+          defaultSize="75%"
+          className=" rounded-xl  w-full h-100 my-4 "
+        >
+          <p className="text-[28px]  font-bold  m-3  text-mauve-600  mx-4">
+            Preview{" "}
           </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+          {/* For render HTML and CSS */}
+          <iframe
+            width="100%"
+            height="300px"
+            frameBorder={0}
+            sandbox="allow-scripts"
+            srcDoc={srcDoc}
+            className="font-semibold"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+            {/* {srcDoc} */}
+          </iframe>
+        </ResizablePanel>
+
+        <ResizableHandle withHandle />
+
+        {/* Console */}
+        <ResizablePanel
+          defaultSize="25%"
+          className="rounded-xl  w-full h-80 my-4 "
+        >
+          <p className="text-[28px]  font-bold  m-3  text-mauve-600  mx-4">
+            Console
+          </p>
+          {/* For render Javascript */}
+          <p className="text-gray-300"></p>
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </div>
   );
 }
